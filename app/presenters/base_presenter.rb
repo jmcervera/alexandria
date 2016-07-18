cf03ecd0b5cc0018bb2a7c46e27f81175c6a0997
Juanma Cervera <juanmacervera@gmail.com>
2016-07-18 08:20:35 +0200

    prepares base presenter for next changes

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,17 +1,35 @@
 class BasePresenter
 
-  # Define a class level instance variable
+  # Define class level instance variables
+  @relations = []
+  @sort_attributes = []
+  @filter_attributes = []
   @build_attributes = []
 
   class << self
 
-    # accessor to the class level instance variable
-    attr_accessor :build_attributes
+    # accessor to the class level instance variables
+    attr_accessor :relations, :sort_attributes,
+                  :filter_attributes, :build_attributes
 
     def build_with(*args)
       @build_attributes = args.map(&:to_s)
     end
+
+    def related_to(*args)
+      @relations = args.map(&:to_s)
+    end
+
+    def sort_by(*args)
+      @sort_attributes = args.map(&:to_s)
+    end
+
+    def filter_by(*args)
+      @filter_attributes = args.map(&:to_s)
+    end
+
   end
+
   attr_accessor :object, :params, :data
 
   def initialize(object, params)
